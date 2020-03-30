9add8459b4797627d5612db40bbb2a38c9558810
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 12:11:34 -0700

    Create ForagerBee subclass

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,12 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+    // Use constructor to create properties
+    constructor() {
+      super();
+      // Set instance properties
+      this.age = 10;
+      this.job = 'find pollen';
+      this.canFly = true;
+      this.treasureChest = [];
+    }
+    forage = (treasure) => {this.treasureChest.push(treasure)};
 };
