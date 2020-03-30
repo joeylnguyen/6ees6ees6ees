f57892d2484cac2eacfea136780d1711c3a532e8
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 12:11:44 -0700

    Create RetiredForagerBee subclass

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+// Set constructor
+constructor() {
+  // Use super to inherit properties and methods
+  super();
+  // Set instance properties
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+};
+// Add forage method
+forage = () => {return "I am too old, let me play cards instead"};
+// Add gamble method
+gamble = (treasure) => {this.treasureChest.push(treasure)};
 };
