f31c226af6f1c52fdcfecd72484cfe23d22130bf
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 11:49:28 -0700

    Refactor eat method to ES6 format

--- src/Grub.js
+++ src/Grub.js
@@ -1,10 +1,10 @@
 class Grub {
+  // Set constructor to set instance properties
   constructor() {
     this.age = 0;
     this.color = 'pink';
     this.food = 'jelly';
   }
-  eat() {
-    return 'Mmmmmmmmm jelly';
-  }
+  // Add eat method
+  eat = () => {return 'Mmmmmmmmm jelly'};
 };
