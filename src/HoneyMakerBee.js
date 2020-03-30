87b37d75717350d0c4103c83689feb0164571618
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 11:53:04 -0700

    Create HoneyMakerBee subclass

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    // Use super to inherit properties
+    super()
+    // Set instance properties
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  };
+
+  // Add makeHoney method
+  makeHoney = () => {this.honeyPot++};
+  // Add giveHoney method
+  giveHoney = () => {this.honeyPot--};
 };
