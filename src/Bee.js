636b1c5a234a3b258efd6118da6467535b9d33ea
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 11:43:47 -0700

    Create Bee subclass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
-  // TODO..
+// Use extend to inherit from Grub
+class Bee extends Grub {
+  // Set initial properties
+  constructor() {
+    // Use super to inherit methods and properties of Grub
+    super()
+    // Set unique properties
+    this.age = 5
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+  }
 };
